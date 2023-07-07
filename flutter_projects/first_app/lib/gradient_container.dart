3ca4d574ac34131155f62febef08b911049dbda3
Philip Spelman <pspelman@autofi.io>
2023-07-07 08:48:18 -0400

    - abstracted out gradient_container.dart - abstracted out styled_text.dart - set colors as a variable and text as a variable

---
+++ flutter_projects/first_app/lib/gradient_container.dart
@@ -0,0 +1,50 @@
+import 'dart:ffi';
+
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText('BRING ON LOS JOKOS'),
+      ),
+    );
+  }
+}
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+//
+//   final List<Color> colors;
+//
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: Alignment.topCenter,
+//           end: Alignment.bottomRight,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('BRING ON LOS JOKOS'),
+//       ),
+//     );
+//   }
+// }
+
--- flutter_projects/first_app/lib/main.dart
+++ flutter_projects/first_app/lib/main.dart
@@ -1,64 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'gradient_container.dart';
+
 void main() {
   // runApp(const MyApp());
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 106, 236, 238),
-                Colors.deepPurpleAccent
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello world and such',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(Colors.pinkAccent, Colors.lightBlue),
       ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
